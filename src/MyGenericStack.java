c6930c7dcf9461b739c59c54e6723122bf9bd01d
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-08-18 11:18:46 +0700

    18/08

---
+++ src/MyGenericStack.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList();
+    }
+    public void push(T element) {
+        stack.addFirst(element);
+    }
+    public T pop() {
+        if (isEmpty() == true) {
+            System.out.println("Fail!");
+        }
+        return stack.removeFirst();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
